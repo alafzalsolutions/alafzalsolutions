8e1f11447a754ccfa562abb6afb619a4f8277b7a
alafzalsolutions <alafzal611@gmail.com>
2026-04-11 22:23:28 +0500

    first commit of my stationery site

---
+++ assets/JS/script.js
@@ -0,0 +1,5 @@
+// Declaration and Initiallization of a variable.
+var name; // Declaration of a variable.
+var name = "Umair Ahmed"; // Both Declare and Initialize. // Initiallize actaually means that giving the first value of the variable.
+
+
